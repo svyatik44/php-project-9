66c6c21f4c5cb452327359d97b3983f349f3474d
svyatik44 <ukrainecsvatoslav85@gmail.com>
2023-02-09 23:17:47 +0300

    templates

--- public/index.php
+++ public/index.php
@@ -3,12 +3,19 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
+$app->get('/users/{id}', function ($request, $response, $args) {
+    
+    return $this->get('renderer')->render($response, 'index.html', $params);
 });
+
+
 $app->run();
