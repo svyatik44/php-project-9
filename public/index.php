2f34949197cebb337ccf4c2824ad23a9f143f005
svyatik44 <ukrainecsvatoslav85@gmail.com>
2023-02-09 20:18:39 +0300

    github actions

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-// Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
@@ -11,7 +10,5 @@ $app->addErrorMiddleware(true, true, true);
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
 });
 $app->run();
